ad5496cf8cdc24b07e4a0ee3df7eb7255de113f2
Nowocinski <vito24222@gmail.com>
2022-06-15 14:51:23 +0200

    Added ButterKnife

--- app/src/main/java/com/example/podcastplayer/LoginActivity.java
+++ app/src/main/java/com/example/podcastplayer/LoginActivity.java
@@ -2,13 +2,35 @@ package com.example.podcastplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.emailEditText)
+    EditText emailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText passwordEditText;
+    @BindView(R.id.loginButton)
+    Button loginButton;
+    @BindView(R.id.registerButton)
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.registerButton)
+    public void registerClicked() {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
