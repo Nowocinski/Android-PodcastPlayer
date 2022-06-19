c9d7d29fa3382c49c5e1628021b32068f796ec54
Nowocinski <vito24222@gmail.com>
2022-06-19 09:37:24 +0200

    Remove Title Bar/Toolbar

--- app/src/main/java/com/example/podcastplayer/screens/login/LoginActivity.java
+++ app/src/main/java/com/example/podcastplayer/screens/login/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.podcastplayer.screens.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -39,6 +40,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         // TODO: W przyszłości tworzenie obiektu powinno odbywać się przy pomocy dependency injection.
